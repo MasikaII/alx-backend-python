b2c672632b04b25b86994c73a0e84debda6e1eab
Stephy <atienostephanie040@gmail.com>
2023-04-04 12:30:38 +0000

    runtime

---
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"""
+using asyncio.gather
+"""
+
+
+import asyncio
+import time
+async_comprehension = __import__('1-async_comprehension').async_comprehension
+
+
+async def measure_runtime() -> float:
+    """
+    Measures and returns total runtime
+    """
+    begin = time.perf_counter()
+    chore = [asyncio.create_task(async_comprehension()) for i in range(4)]
+    result = await asyncio.gather(*chore)
+    aggregate_time = time.perf_counter() - start
+    return aggregate_time
